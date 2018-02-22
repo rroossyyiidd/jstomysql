5c7b83f46e5752c67ee5a502dea7a2e29754bc9b
rroossyyiidd <rroossyyiidd@gmail.com>
2018-02-22 11:02:02 +0700

    update

--- exportToJson.js
+++ exportToJson.js
@@ -8,31 +8,42 @@ var con = mysql.createConnection({
     database: "wilayah"
 });
 
-var data = fs.readFileSync('databaru.json', 'utf8');
-var dataLokasi = JSON.parse(data);
+var databps = fs.readFileSync('databaru.json', 'utf8');
+var dataLokasi = JSON.parse(databps);
 
 con.connect(function (err) {
     if (err) throw err;
     con.query("SELECT * FROM data_wilayah", function (err, result, fields) {
         if (err) throw err;
         data = result;
+        var jml = 1;
+        const newLokasi = dataLokasi.map(value => {
+            console.log(jml + " data(s) updated");
 
-        const newLokasi = dataLokasi.map(provinsi=>{
+            jml = jml + 1;
             return {
-                ...provinsi,
-                kota:provinsi.kota.map(kota=>{
+                type: "Provinsi",
+                ...value,
+                kota: value.kota.map(value2 => {
+                    var cek = data.find(item => item.bps_id_kota == value2.id);
                     return {
-                        ...kota,
-                        rajaongkir_id:datarajaongkir.find(item=>item.kota_id===kota.id).raja_city_id
+                        ...value2,
+                        rajaongkir_id_city: cek['raja_id_city'],
+                        kecamatan: value2.kecamatan.map(value3 => {
+                            return {
+                                ...value3,
+                                type: "Kecamatan"
+                            }
+                        })
                     }
                 })
             }
-        })
+        });
 
-        // var str = JSON.stringify(datas);
-        // fs.writeFile('datajson.txt', datas, function (err) {
-        //     if (err) throw err;
-        //     console.log('Saved!');
-        // });
+        var str = JSON.stringify(newLokasi);
+        fs.writeFile('datajson.json', str, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
     });
 });
